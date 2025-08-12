bad3cf6b8ce0fc0bdc6fe9742bb21de6e0361a6a
Harsh Kharwar <1harshkharwar@gmail.com>
2025-08-12 10:55:18 +0530

    Initial Hello World Flask app commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # This is the line we will modify later
+    return 'Hello, World from main!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
